d0a03ab63cf751f50d41f46b20f01196897f3253
Akanksha10029 <akanksharani405@gmail.com>
2024-09-07 15:11:37 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      screens: {
+        'xxs': '390px', // Defining for very small screens (e.g., 420px wide)
+        'xxxs': '240px',
+      },
     },
   },
   plugins: [],
