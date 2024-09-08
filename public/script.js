3ad442f249361acc033160ae8b529cff60fd3f9c
Akanksha10029 <akanksharani405@gmail.com>
2024-09-08 22:29:18 +0530

    changes

--- public/script.js
+++ public/script.js
@@ -32,34 +32,34 @@ customCheckboxes.forEach((checkbox, index) => {
 
 
 
-// let completedGoals = 0; // Track the number of completed goals
+let completedGoals = 0; // Track the number of completed goals
 
-// // Attach event listeners to each goal button
-// document.getElementById('goal1').addEventListener('click', function() { updateProgress(this); });
-// document.getElementById('goal2').addEventListener('click', function() { updateProgress(this); });
-// document.getElementById('goal3').addEventListener('click', function() { updateProgress(this); });
+// Attach event listeners to each goal button
+document.getElementById('goal1').addEventListener('click', function() { updateProgress(this); });
+document.getElementById('goal2').addEventListener('click', function() { updateProgress(this); });
+document.getElementById('goal3').addEventListener('click', function() { updateProgress(this); });
 
 
-// function updateProgress(button) {
-//     if (!button.disabled) { // Check if the button is not already pressed
-//         completedGoals++;
-//         progress += 100 / totalGoals; // Increment progress by one-third
-//         progressBar.style.width = progress + '%';
-//         button.disabled = true; // Disable the button after it's clicked to prevent multiple clicks
+function updateProgress(button) {
+    if (!button.disabled) { // Check if the button is not already pressed
+        completedGoals++;
+        progress += 100 / totalGoals; // Increment progress by one-third
+        progressBar.style.width = progress + '%';
+        button.disabled = true; // Disable the button after it's clicked to prevent multiple clicks
 
-//         progressBar.textContent = `
-//         ${completedGoals} / ${totalGoals} completed`
+        progressBar.textContent = `
+        ${completedGoals} / ${totalGoals} completed`
         
         
-//         if(completedGoals!=totalGoals){
-//             set_all_three.classList.remove('hidden')
-//         }
+        if(completedGoals!=totalGoals){
+            set_all_three.classList.remove('hidden')
+        }
 
-//         else{
-//             set_all_three.classList.add('hidden')
-//             // Showing completion message when all goals are completed
-//             goalStatus.classList.remove('hidden')
-//         }
-//     }    
+        else{
+            set_all_three.classList.add('hidden')
+            // Showing completion message when all goals are completed
+            goalStatus.classList.remove('hidden')
+        }
+    }    
 
-// }
+}
